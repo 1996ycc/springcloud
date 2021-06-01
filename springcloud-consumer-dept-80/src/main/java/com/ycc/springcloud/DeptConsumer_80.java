1beaf2987700ea069ecb811159a337f5d40b7b4e
YCC <1>
2021-06-01 22:41:10 +0800

    2021/6/1 ycc 自定义ribbbon

--- springcloud-consumer-dept-80/src/main/java/com/ycc/springcloud/DeptConsumer_80.java
+++ springcloud-consumer-dept-80/src/main/java/com/ycc/springcloud/DeptConsumer_80.java
@@ -1,5 +1,6 @@
 package com.ycc.springcloud;
 
+import com.ycc.myrule.TestMyRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,6 +8,7 @@ import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RibbonClient(name = "SPRINGCLOUD-PROVIDER-DEPT",configuration = TestMyRule.class)
 public class DeptConsumer_80 {
     public static void main(String[] args) {
         SpringApplication.run(DeptConsumer_80.class,args);
--- springcloud-consumer-dept-80/src/main/java/com/ycc/springcloud/config/ConfigBean.java
+++ springcloud-consumer-dept-80/src/main/java/com/ycc/springcloud/config/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.ycc.springcloud.config;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +10,12 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ConfigBean { // @Configuration  -- spring applicationContext.xml
     // 配置负载均衡  实现RestTemplate
+    // IRule
+    // RandomRule     随机
+    // RoundRobinRule 轮询
+    // RetryRule  会先按照轮询获取服务，如果失败，在指定时间内进行重试
+    // AvailabilityFilteringRule : 会过滤掉跳闸访问故障的服务，对剩下的服务进行轮询
+
     @Bean
     @LoadBalanced  //Ribbon
     public RestTemplate getRestTemplate(){
